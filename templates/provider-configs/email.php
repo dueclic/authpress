943c5efe58bffd9362208be881135fe91ef34bf1
faghy <christian.serafino@dueclic.com>
2025-08-23 11:46:59 +0200

    SCADENZA TOKEN: aggiunto campo di input a provider-configs/email.php

--- templates/provider-configs/email.php
+++ templates/provider-configs/email.php
@@ -4,7 +4,25 @@
  * @var \AuthPress\Providers\Abstract_Provider $provider
  * @var array $providers
  */
+
+$email_settings = $providers['email'] ?? [];
+$token_duration = $email_settings['token_duration'] ?? 20; // Default to 20 minutes
 ?>
+
+<div class="ap-form__group">
+	<label class="ap-label" for="email_token_duration"><?php _e("Durata token:", "two-factor-login-telegram"); ?></label>
+	<p class="ap-provider-config__description">
+		<?php _e("Durata predefinita token in minuti", "two-factor-login-telegram"); ?>
+	</p>
+	<div class="ap-form">
+		<div class="ap-form__group">
+			<input id="email_token_duration" class="ap-input"
+			       name="wp_factor_providers[email][token_duration]"
+			       type="number"
+			       value="<?php echo esc_attr($token_duration); ?>"/>
+		</div>
+	</div>
+</div>
 
 <div class="ap-form__group">
 	<label class="ap-label"><?php _e("Mail testing", "two-factor-login-telegram"); ?></label>
